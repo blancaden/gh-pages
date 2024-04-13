a950bc4789c01637597a93a30a426e91be418327
Blanca <blankdgonzfigue@gmail.com>
2024-04-13 11:18:57 +0200

    change URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: " https://blancaden.github.io/gh-pages/",
+  base: " https://blancaden.github.io/github-pages/",
 });
